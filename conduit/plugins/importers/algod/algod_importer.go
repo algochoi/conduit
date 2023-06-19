4923b042689626452fc8853dfe92a8059bf168f5
algochoi <86622919+algochoi@users.noreply.github.com>
2023-06-18 23:23:19 -0400

    Test without getting status

--- conduit/plugins/importers/algod/algod_importer.go
+++ conduit/plugins/importers/algod/algod_importer.go
@@ -420,11 +420,11 @@ func (algodImp *algodImporter) GetBlock(rnd uint64) (data.BlockData, error) {
 	var status models.NodeStatus
 	var blk data.BlockData
 
-	importStart := time.Now()
-
 	for r := 0; r < retries; r++ {
 
-		status, err = algodImp.aclient.StatusAfterBlock(rnd - 1).Do(algodImp.ctx)
+		importStart := time.Now()
+
+		// status, err = algodImp.aclient.StatusAfterBlock(rnd - 1).Do(algodImp.ctx)
 		algodImp.logger.Tracef("importer algod.GetBlock() called StatusAfterBlock(%d) err: %v", rnd-1, err)
 
 		algodImp.logger.Infof("GetBlock Start: %v err: %v", time.Since(importStart), err)
