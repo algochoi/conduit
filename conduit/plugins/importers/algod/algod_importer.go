d4493f51365ac38f661382b1256d0572edaa342a
algochoi <86622919+algochoi@users.noreply.github.com>
2023-06-20 11:39:35 -0400

    Go back 1 round

--- conduit/plugins/importers/algod/algod_importer.go
+++ conduit/plugins/importers/algod/algod_importer.go
@@ -424,7 +424,7 @@ func (algodImp *algodImporter) GetBlock(rnd uint64) (data.BlockData, error) {
 
 		importStart := time.Now()
 
-		status, err = algodImp.aclient.StatusAfterBlock(rnd - 1).Do(algodImp.ctx)
+		status, err = algodImp.aclient.StatusAfterBlock(rnd - 2).Do(algodImp.ctx)
 		algodImp.logger.Tracef("importer algod.GetBlock() called StatusAfterBlock(%d) err: %v", rnd-1, err)
 
 		algodImp.logger.Infof("GetBlock Start: %v err: %v", time.Since(importStart), err)
